b576b7f8d0e07d1d1cb30eea110d961a0e6d49f7
ddout <ddout@qq.com>
2018-05-29 00:31:30 +0800

    test connit

---
+++ okooo/img/img_yzm.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+
+import base64, urllib, urllib2
+import json
+
+
+def getImgBase64(path):
+    with open(path, "rb") as f:
+        # b64encode是编码，b64decode是解码
+        base64_data = base64.b64encode(f.read())
+        # base64.b64decode(base64data)
+    return base64_data
+
+
+def getYZM(img_base64):
+    host = 'http://txyzmsb.market.alicloudapi.com'
+    path = '/yzm'
+    method = 'POST'
+    appcode = ''
+    querys = ''
+    bodys = {}
+    url = host + path
+
+    bodys['v_pic'] = img_base64
+    bodys['v_type'] = '''ne5'''
+    post_data = urllib.urlencode(bodys)
+    request = urllib2.Request(url, post_data)
+    request.add_header('Authorization', 'APPCODE ' + appcode)
+    # 根据API的要求，定义相对应的Content - Type
+    request.add_header('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
+    response = urllib2.urlopen(request)
+    content = response.read()
+    if (content):
+        data = json.loads(content)
+        if data.get("errCode") == 0:
+            return data.get("v_code")
+
+
--- okooo/test.py
+++ okooo/test.py
@@ -1,38 +1,4 @@
 # -*- coding: utf-8 -*-
-import json
-import time
+from okooo.img.img_yzm import getImgBase64, getYZM
 
-import psycopg2
-import psycopg2.pool
-import psycopg2.extras
-
-__database = "db_ddout"
-__dbuser = "ddout"
-__password = "ddout"
-__host = "192.168.56.101"
-__port = "5432"
-#
-dbpool = psycopg2.pool.SimpleConnectionPool(1, 10, dbname=__database, user=__dbuser, host=__host, password=__password,
-                                            port=__port)
-conn = dbpool.getconn()
-cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-try:
-    sql1 = """
-    insert into okooo.test(id,name,bri,tjson)
-    values(%s,%s,%s,%s);
-    """
-    cursor.execute(sql1, (1, None, "2013-04-03 18:00".replace(" "," "),
-                          '{"name":"a","age":"12","child":{"name":"a","age":"12"}, "childen":[{"name":"a","age":"12"},{"name":"a","age":"12"}]}'))
-    conn.commit()
-    #
-    sqlstr = "select * from okooo.test"
-    cursor.execute(sqlstr)
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row["bri"])
-except Exception, e:
-    print(e)
-finally:
-    dbpool.putconn(conn)
-
-dbpool.closeall()
+print(getYZM(getImgBase64("/Users/mac/Downloads/captcha.jpg")))
